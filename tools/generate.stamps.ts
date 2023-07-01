405e5413e1820c60dc521c935e61abe9ea65a7c4
PierreLevres <62832923+PierreLevres@users.noreply.github.com>
2023-07-01 15:08:48 +0200

    New AppId for Kia

--- tools/generate.stamps.ts
+++ tools/generate.stamps.ts
@@ -8,6 +8,7 @@ const SIZE = 1000;
 const appsToGenerate = [
   { brand: 'hyundai', appId: '014d2225-8495-4735-812d-2616334fd15d', fileName: 'hyundai-014d2225-8495-4735-812d-2616334fd15d', image: 'hacksore/hks:arm' },
   { brand: 'kia', appId: 'e7bcd186-a5fd-410d-92cb-6876a42288bd', fileName: 'kia-e7bcd186-a5fd-410d-92cb-6876a42288bd', image: 'hacksore/hks:arm' },
+  { brand: 'kia', appId: 'a2b8469b-30a3-4361-8e13-6fceea8fbe74', fileName: 'kia-a2b8469b-30a3-4361-8e13-6fceea8fbe74', image: 'hacksore/hks:arm' },
   { brand: 'hyundai', appId: '99cfff84-f4e2-4be8-a5ed-e5b755eb6581', fileName: 'hyundai', image: 'hacksore/hks:latest' },
   { brand: 'kia', appId: '693a33fa-c117-43f2-ae3b-61a02d24f417', fileName: 'kia', image: 'hacksore/hks:latest' },
 ];
